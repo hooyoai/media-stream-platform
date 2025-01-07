f5b7c3e591177afa7f483088bc575e1e32cdb1ae
hooyoai <143280804+hooyoai@users.noreply.github.com>
2025-01-06 21:08:31 -0500

    Add video player screen

---
+++ mobile/lib/features/video/video_player_screen.dart
@@ -0,0 +1,57 @@
+// File: mobile/lib/features/video/video_player_screen.dart
+// Video player screen with HLS support
+
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class VideoPlayerScreen extends StatefulWidget {
+  final String videoUrl;
+  final String title;
+
+  const VideoPlayerScreen({
+    Key? key,
+    required this.videoUrl,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  _VideoPlayerScreenState createState() => _VideoPlayerScreenState();
+}
+
+class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
+  late VideoPlayerController _controller;
+  bool _isInitialized = false;
+
+  @override
+  void initState() {
+    super.initState();
+    // TODO: Initialize video player controller
+    // TODO: Setup HLS stream
+  }
+
+  @override
+  void dispose() {
+    // TODO: Cleanup video player
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: _isInitialized
+            ? AspectRatio(
+                aspectRatio: _controller.value.aspectRatio,
+                child: VideoPlayer(_controller),
+              )
+            : const CircularProgressIndicator(),
+      ),
+      // TODO: Add video controls overlay
+      // TODO: Add quality selection
+      // TODO: Add fullscreen support
+    );
+  }
+}
