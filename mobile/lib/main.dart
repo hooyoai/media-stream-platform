5d588db1d57ed77e789216e0eb240287c391b1ae
hooyoai <143280804+hooyoai@users.noreply.github.com>
2025-01-06 21:08:09 -0500

    Add Flutter main entry point

---
+++ mobile/lib/main.dart
@@ -0,0 +1,32 @@
+// File: mobile/lib/main.dart
+// Main entry point for the Flutter application
+
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MediaStreamApp());
+}
+
+class MediaStreamApp extends StatelessWidget {
+  const MediaStreamApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Media Stream',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const AuthWrapper(), // TODO: Implement AuthWrapper
+    );
+  }
+}
+
+// TODO: Implement AuthWrapper class
+// TODO: Setup route configuration
+// TODO: Add state management setup
